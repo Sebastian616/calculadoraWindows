d8e2509eace82651729d74ee46f580ec0e7ceaca
Miguel <miguelitovasquezo@gmail.com>
2023-04-17 17:44:57 -0500

    first commit

---
+++ calculadora_1/src/operaciones/division.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author SENA
+ */
+public class division {
+    private int n1;
+    private int n2;
+
+    public division(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public int mostrarDivision() {
+        int rDivision;
+        rDivision = n1 / n2;
+        return rDivision;
+    }
+    
+}
---
+++ calculadora_1/src/operaciones/multiplicacion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author SENA
+ */
+public class multiplicacion {
+    
+    private int n1;
+    private int n2;
+
+    public multiplicacion(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public int mostrarMulti() {
+        int rMulti;
+        rMulti = n1 * n2 ;
+        return rMulti;
+    }
+    
+    
+}
